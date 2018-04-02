ed716292ba2be3fb1fd2ecf81012b3e7cc3162fb
D7702_09 <d7702_09@D7702-09ui-iMac.local>
2018-04-02 15:48:05 +0900

    fff

--- Aliem Image View/ViewController.swift
+++ Aliem Image View/ViewController.swift
@@ -1,25 +1,39 @@
-//
-//  ViewController.swift
-//  Aliem Image View
-//
-//  Created by D7702_09 on 2018. 4. 2..
-//  Copyright © 2018년 lse. All rights reserved.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var myImageView: UIImageView!
+    @IBOutlet weak var index: UILabel!
+    
+    var direction = 1 // direction이 1이면 증가 0이면 감소 구분
+    var counter = 1 // 증가나 감소 여부
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        // 첫번째 이미지를 로드함.
+        myImageView.image = UIImage(named: "frame1.png")
+        index.text = "1"
     }
+    
+    @IBAction func imageUpdate(_ sender: Any) {
+        //counter가 5이면 감소시키 위해 direction을 0으로 변경
+        if counter == 5 {
+            direction = 0
+        }
+        //counter가 1이면 증가시키 위해 direction을 1으로 변경
+        else if counter == 1 {
+            direction = 1
+        }
+        //direction이 1이면 counter를 1씩 증가
+        if direction == 1 {
+            counter += 1 }
+        //direction이 0이면 counter를 1씩 감소
+        else if direction == 0 {
+            counter = counter - 1
+        }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        myImageView.image = UIImage(named: "frame\(counter).png")
+        index.text = String(counter)
     }
-
-
 }
 
